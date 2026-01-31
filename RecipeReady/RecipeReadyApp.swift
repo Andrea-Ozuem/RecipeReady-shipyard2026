65edf9427b9d0275668ad26ec6d439e6e3d1ec1e
Andrea-Ozuem <andreaozuem2021@gmail.com>
2026-01-31 01:54:19 +0100

    Initial Commit

---
+++ RecipeReady/RecipeReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeReadyApp.swift
+//  RecipeReady
+//
+//  Created by Ozuem Andrea Chukwunomswe  on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct RecipeReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
